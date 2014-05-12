e490997457ce0d35c4b43fecd9d225eb51bcac53
GitMiku <MikuMiku@lavabit.com>
2014-05-12 05:16:49 +0200

    Create scanlist.sh

---
+++ scanlist.sh
@@ -0,0 +1,21 @@
+#Filename: scanlist.sh
+#Purpose: Scans a list of IP:PORT
+
+list=$1 #the list file is the first command line argument
+#The list should have one proxy per line in IP:PORT format
+
+if [ -z "$1" ]
+then
+    echo "Usage: This program scans a list of IPs on specific ports"
+    echo "$0 [list]"
+    echo "Where list is the name of the list file. The list should have one IP per line and be in IP:PORT format"
+else
+
+    resultfile=scanlist$(date "+%m%d%y%H%M%S").txt
+    touch $resultfile
+    for i in $(cat $list)
+    do
+        results=$(nmap -p$(echo $i | cut -d: -f2) $(echo $i | cut -d: -f1) -Pn | grep $(echo $i | cut -d: -f2))
+        echo "IP: $i || Scan Results: $results" >> $resultfile
+    done
+fi
